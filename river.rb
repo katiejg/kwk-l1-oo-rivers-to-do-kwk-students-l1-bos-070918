30ca5e29e90e1f653a671440a5a4b12e93d5e91b
katiejg <jng.katie@gmail.com>
2018-07-22 22:39:53 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,25 @@
 # river.rb
+class River 
+  attr_accessor :length, :countries, :discharge
+  def initialize(name)
+    @name=name
+    @lenth=length
+    @countries=countries
+  end
+  def name
+    @name
+  end
+  def name=(name)
+    @name=name
+  end
+  def flood
+    "#{self.discharge}+#{self.discharge}/100*3"
+  end
+  def dry_up
+    self.discharge / 2
+  end
+end
+
+thing = River.new("idk")
+thing.discharge = 10
+thing.flood
